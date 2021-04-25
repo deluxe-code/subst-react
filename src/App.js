378eac4cfce1d6624677b22b82228868ffefed53
jacoballessio <jacob.allessio@gmail.com>
2021-04-25 12:05:52 -0700

    Revert "Merge?"
    This reverts commit f8ac7f0bcdfa56320ba6bfad7d08105ff7c249e7, reversing
    changes made to de7ce600d94c11d0b45aa01d11d1de980e6fda60.

--- src/App.js
+++ src/App.js
@@ -4,38 +4,15 @@ import Navbar from "./Navbar.jsx"
 import Home from "./Home.jsx"
 import Signup from "./Signup.jsx"
 import Login from "./Login.jsx"
-<<<<<<< HEAD
 import DrugInfo from "./DrugInfo.jsx"
 import AddDosePage from "./AddDosePage.jsx"
 import CreateSchedulePage from "./CreateSchedulePage.jsx"
-=======
-import firebase from "firebase";
->>>>>>> f89abbd5bfed5727f13da51d59c74f1c2747fb7e
 // import Settings from "./Settings.jsx"
 import "./App.css";
 
 
-<<<<<<< HEAD
 function App() {
   let vibe = "yes";
-=======
-
-export default function App() {
-  const firebaseApp = firebase.apps[0];
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (!user) {
-        console.error(
-          'User is not signed in!'
-        );
-      } else {
-        console.log("User is signed in??");
-      }
-    });
-  });
-
->>>>>>> f89abbd5bfed5727f13da51d59c74f1c2747fb7e
   return (
     <Router>
     <Navbar />
@@ -48,7 +25,7 @@ export default function App() {
       <Route path="/create_schedule" component={CreateSchedulePage} />
     </Switch>
     </Router>
-
   );
 }
 
+export default App;
