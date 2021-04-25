11e5e9279d34ef809fe7226d58bc6daf4d8865e5
Ethan N <ethannikolas@gmail.com>
2021-04-25 13:07:54 -0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from 'firebase/app';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyB0YksSwW1EB788GmuDuZpO2tTKJAezHpU",
+  aauthDomain: "subst-app.firebaseapp.com",
+  databaseURL: "https://subst-app.firebaseio.com",
+  projectId: "subst-app",
+  storageBucket: "subst-app.appspot.com",
+  messagingSenderId: "172488889933",
+  appId: "1:172488889933:web:9f9bee754b15f31a1c6e04",
+  measurementId: "G-VGQR7410B4"
+};
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
