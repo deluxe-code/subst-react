8d8bfe88ebab696453a7fd5e7425f0896b7c6049
Ethan N <ethannikolas@gmail.com>
2021-05-03 12:10:28 -0700

    Implement javascript stylesheets for navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,36 @@
 import React, { useState, useRef, useEffect } from "react"
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import searchIcon from './assets/img/search_white_24dp.svg'
 import firebase from 'firebase';
 
 export default function Navbar() {
+  let [currentPage, setCurrentPage] = useState([]);
+  
+  useEffect(() => {
+    console.log("Time to update currentPage");
+    // Let's color the page they are on?
+
+  }, [currentPage])
+  const styles = {
+    nav: {
+      width: '100%',
+      height: '60px',
+      lineHeight: '60px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: 'black',
+      position: 'fixed',
+      bottom: '0px'
+    },
+    nav_link: {
+      width: '100%',
+      color: 'lightGray',
+      textDecoration: 'none',
+      lineHeight: '25px'
+    }
+  }
+  
   let authLabel = useRef();
     firebase.auth().onAuthStateChanged(function(user) {
       let currentStatus = authLabel.current;
@@ -15,24 +42,24 @@ export default function Navbar() {
     });
   
   return (
-    <nav>
-      <Link to="/">
-      <a href="#">Home</a>
+    <nav style={styles.nav}>
+      <Link style={styles.nav_link} onClick={setCurrentPage} to="/">
+      <span>Home</span>
       </Link>
-      <Link to="/login">
-      <a href="#">Login</a>
+      <Link style={styles.nav_link} to="/login">
+      <span>Login</span>
       </Link>
-      <Link to="/signup">
-      <a href="#">Signup</a>
+      <Link style={styles.nav_link} to="/signup">
+      <span>Signup</span>
       </Link>
-      <Link to="/search">
+      <Link style={styles.nav_link} to="/search">
         <img src={searchIcon} className="search-icon"/>
       </Link>
-      <Link to="/account">
-      <a href="#">Account</a>
+      <Link style={styles.nav_link}to="/account">
+      <span>Account</span>
       </Link>
-      <Link to="/create_schedule">
-      <a href="#">Create Schedule</a>
+      <Link style={styles.nav_link} to="/create_schedule">
+      <span>Create Schedule</span>
       </Link>
       
       <div ref={authLabel}></div>
